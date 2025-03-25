63406623fbfc6b806937ceae8471aa4819ad048f
iago dorea <iago@DESKTOP-7K3B6OJ.>
2025-03-25 18:11:16 -0300

    Added old BRLL code

--- src/peque.cr
+++ src/peque.cr
@@ -1,3 +1,35 @@
 def output(oput);
 	  puts oput
   end
+  def listen;
+	gets.chomp
+end
+def iseql(um,dozz,codigo);
+	if um == dozz
+	codigo
+	end
+end
+def ismot(mtum,mtdozz,mtcod);
+	if mtum >> mtdozz
+	mtcod
+	end
+end
+def ismit(mium,midozz,micod);
+	if mium << midozz
+	micod
+	end
+end
+def isneql(neum,nedozz,necod);
+	if neum != nedozz
+	necod
+	end
+end
+def exec(exc);
+system(exc)
+end
+def rn(rnum);
+rand(rnum)
+end
+def wait(wnm)
+	sleep wnm
+end
